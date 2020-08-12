abe4a935b749377c3e9d62b2d064edf46f700510
torreseam <615694479+torreseam@users_noreply.github.com>
2020-08-11 22:43:49 -0500

    global store and test

---
+++ client/src/utils/Global.State.js
@@ -0,0 +1,25 @@
+//Create a New Context Object
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value= [], ...props}) => {
+    const [state,dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currenCategory: '',
+    });
+    //use this to confirm that it works!
+    console.log(state);
+    return<Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+
+export { StoreProvider, useStoreContext };
---
+++ client/src/utils/actions.js
@@ -0,0 +1,12 @@
+
+
+
+
+//use by productslist 
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+
+//take the list of cat retrieved from the server and store it in Global State
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+
+//allos to select category actions and display products created from product list
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
